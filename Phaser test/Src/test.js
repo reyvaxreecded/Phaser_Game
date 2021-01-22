d18d44afd36c612eff1f09c7a91c434b61a2b115
Xderedec <xavier.deredec@live.fr>
2021-01-22 11:18:00 +0100

    22/01

--- Phaser test/Src/test.js
+++ Phaser test/Src/test.js
@@ -1,15 +1,15 @@
 // import * as Phaser from '../node_modules/phaser/dist/phaser.js';
 export class Game{
     constructor(){
+        let bulletTime = 0;
         var config = {
             type: Phaser.AUTO,
-            width: 1280,
-            height: 920,
+            width: 1024,
+            height: 768,
             physics: {
                 default: 'arcade',
                 arcade: {
-                    gravity: { y: 300 },
-                    debug: false
+                    debug: true
                 }
             },
             scene: {
@@ -26,15 +26,24 @@ export class Game{
         this.load.image('sky', 'assets/sky.png');
         this.load.image('ground', 'assets/platform.png');
         this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 85, frameHeight: 95 });
+        this.load.image('bullet', 'assets/bullet.png');
     }
     create ()
     {
-        this.add.image(400, 500, 'sky');
+        this.bullets = this.add.group();
+        this.bullets.enableBody = true;
+        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        this.bullets.createMultiple(30, 'bullet');
+        this.bullets.setAll('anchor.x', 0.5);
+        this.bullets.setAll('anchor.y', 1);
+        this.bullets.setAll('outOfBoundesKill', true);
+        this.bullets.setAll('checkWorldBounds', true);
+
+        this.fire = this.input.keyboard.addKey(this.Phaser.Keyboard.SPACEBAR);
+        this.sky = this.add.tileSprite(512,384,1921,1080,'sky');
         this.platforms = this.physics.add.staticGroup();
 
-        this.platforms.create(400, 820, 'ground').setScale(5).refreshBody();
-
-        this.player = this.physics.add.sprite(1,1, 'dude');
+        this.player = this.physics.add.sprite(1,384, 'dude');
 
         this.player.setCollideWorldBounds(true);
 
@@ -61,7 +70,7 @@ export class Game{
 
         this.anims.create({
             key: 'down',
-            frames: [ { key: 'dude', frame: 5 }],
+            frames: [ { key: 'dude', frame: 14 }],
             frameRate: 20, 
         })
         this.anims.create({
@@ -69,6 +78,20 @@ export class Game{
             frames: [ { key: 'dude', frame: 6 }],
             frameRate: 20,
         })
+
+        this.anims.create({
+            key:'downl',
+            frames: this.anims.generateFrameNumbers('dude', { start: 14, end: 17 }),
+            frameRate: 7,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key:'downr',
+            frames: this.anims.generateFrameNumbers('dude', { start: 11, end: 13 }),
+            frameRate: 7,
+            repeat: -1
+        })        
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -78,38 +101,59 @@ export class Game{
     update ()
     {
 
+        this.sky.tilePositionX += 2;
+
         if (this.cursors.left.isDown)
         {
             this.player.setVelocityX(-100);
 
-            this.player.anims.play('left', true);
+            this.player.anims.play('down', true);
         }
         else if (this.cursors.right.isDown)
         {
             this.player.setVelocityX(100);
 
-            this.player.anims.play('right', true);
+            this.player.anims.play('down', true);
         }
         else if (this.cursors.down.isDown){
+
+            this.player.setVelocityY(+100);
+
             this.player.anims.play('down');
         }
         else
         {
             this.player.setVelocityX(0);
 
-            this.player.anims.play('turn', true);
+            this.player.anims.play('down', true);
         }
 
-        if (this.cursors.up.isDown && this.player.body.touching.down)
+        if (this.cursors.up.isDown)
         {
-            this.player.setVelocityY(-200);
+            this.player.setVelocityY(-100);
 
         }
-        if (this.player.body.onFloor()== false){
-            this.player.anims.play('up');
+        if(this.fire.isDown)
+        {
+            this.fireBullet();
         }
+        // if (this.player.body.onFloor()== false){
+        //     this.player.anims.play('up');
+        // }
 
 
     }
+    fireBullet()
+    {
+        if(this.time.now > this.bulletTime){
+            bullet = this.bullets.getFirstExists(false);
 
+            if(this.bullet){
+                this.bullet.reset(this.player.x,this.player.y);
+                this.bullet.body.setVelocityY = -400;
+                this.bulletTime = this.time.now + 200;
+
+            }
+        }
+    }
 }
